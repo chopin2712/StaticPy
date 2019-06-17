04634798bccb981a14b110424bf82302b367358b
Gitea <test123456789@gmail.com>
2019-06-17 17:05:34 +0200

    Not working, but go straight on

--- static.py
+++ static.py
@@ -2,19 +2,24 @@
 import os
 import fileinput
 import linecache
+import shutil
 
+template = 'cmd'
 # File selector
-for root, dirs, files in os.walk("./template"):
+for root, dirs, files in os.walk("template/", template):
     for filename in files:
         print(filename)
 filename = input("What file do you want to use? ")
 
 # Copy the content files to the main output dir
-
+os.system("cd ./template/", template)
+print(os.getcwd())
 # Replace variables from var to selected file.
 i = 1
 while i != 0:
-    os.walk("..")
+    os.system("cd ./template/", template)
+    print(os.getcwd())
+    print(os.system("ls"))
     open("var.txt", "r")
     v = linecache.getline('var.txt', i)
     if v == '': # Nothing as end of vars
@@ -24,7 +29,8 @@ while i != 0:
         a = input("[Type e for getting your editor]")
         with fileinput.FileInput("var.txt", inplace=True, backup='.bak') as file:
             for line in file:
-                os.walk("./template")
+                os.system("cd ./template/", template)
+                print(os.getcwd())
                 open(filename)
                 print(line.replace(v, a), end='')
         i = i + 1
