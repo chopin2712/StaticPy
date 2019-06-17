6a130331fedb0ac26e196bfabf06ea703f489e48
Gitea <test123456789@gmail.com>
2019-06-17 18:17:40 +0200

    MAJOR: adding "path", finally

--- static.py
+++ static.py
@@ -4,22 +4,65 @@ import fileinput
 import linecache
 import shutil
 
+# Will define what directory to use for the os module
+file = open("dir.txt", "w")
+file.write('')
+file.close()
+file = open("dir.txt", "r")
+
+file.close()
+file = open("dir.txt", "a")
+file.write(os.getcwd())
+file.write("/template/")
 template = 'cmd'
+file.write(template)
+file.close()
+
+file = open("dir.txt", "r")
+path = file.read()
+file.close()
+
+os.chdir(path)
+file = open("dir.txt", "w")
+file.write(path)
+file.close()
+
+# template selector, to-do
+template = 'cmd'
+
 # File selector
-for root, dirs, files in os.walk("template/", template):
-    for filename in files:
-        print(filename)
+i = 1
+while i != 0:
+    os.chdir(path)
+    open("templates.txt", "r")
+    l = linecache.getline('templates.txt', i)
+    if l == '':
+        i = 0
+    else:
+        print(l)
+        i = i + 1
+
 filename = input("What file do you want to use? ")
+file = open("dir.txt", "a")
+file.write("/")
+file.write(filename)
+file.close()
+file = open("dir.txt", "r")
+path = file.read()
+print(path)
+
+
 
 # Copy the content files to the main output dir
-os.system("cd ./template/", template)
-print(os.getcwd())
+os.chdir(path)
+
+os.system("ls")
+# Help needed
+
 # Replace variables from var to selected file.
 i = 1
 while i != 0:
-    os.system("cd ./template/", template)
-    print(os.getcwd())
-    print(os.system("ls"))
+    os.chdir(path)
     open("var.txt", "r")
     v = linecache.getline('var.txt', i)
     if v == '': # Nothing as end of vars
@@ -29,8 +72,7 @@ while i != 0:
         a = input("[Type e for getting your editor]")
         with fileinput.FileInput("var.txt", inplace=True, backup='.bak') as file:
             for line in file:
-                os.system("cd ./template/", template)
-                print(os.getcwd())
+                os.chdir(path)
                 open(filename)
                 print(line.replace(v, a), end='')
         i = i + 1
