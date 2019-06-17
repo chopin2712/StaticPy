02f11713446ee8ca987a59884f5fce475a159f05
Gitea <test123456789@gmail.com>
2019-06-17 13:12:43 +0200

    Successful test

--- static.py
+++ static.py
@@ -1,4 +1,13 @@
-loop = 1
-while loop != 0:
-    print("ok")
-    loop = 0
+import linecache
+
+i = 1
+while i != 0:
+    open("var.txt", "r")
+    v = linecache.getline('var.txt', i)
+    if v == '':
+        i = 0
+    else:
+        print("What is the variable ",v)
+        a = input("[Type e for getting your editor]")
+        # Replace v by a in the template document
+        i = i + 1
